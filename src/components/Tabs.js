56f703dc735dd61d68e873a656bb25cfff238ef3
Henrik Grønbech <henrg@stud.ntnu.no>
2018-09-13 14:25:47 +0200

    Add styling

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import "../App.css";
+import "./Tabs.css";
 
 class Tabs extends Component {
   render() {
     return (
-      <div className="Tabs">
+      <div className="Tabs" id="tabBar">
         <button onClick={() => this.props.setTab(1)}>1</button>
         <button onClick={() => this.props.setTab(2)}>2</button>
         <button onClick={() => this.props.setTab(3)}>3</button>
